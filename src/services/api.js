1fd807775d4b454d1a632ad05f0c6d7b0a0b56df
Faouzia Mechhidan <f.mechhidan@interface3.be>
2026-06-12 10:05:09 +0200

    changement dans api

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,65 @@
+// import axios from "axios";
+// import { getDefaultStore } from "jotai";
+// import { tokenAtom } from "../atoms/auth.atom";
+
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL, // 🔥 important pour Vercel
+//   timeout: 10000,
+// });
+
+// // Interceptor pour ajouter le token à chaque requête
+// const store = getDefaultStore();
+
+
+// api.interceptors.request.use(
+//   (config) => {
+//     // token depuis Jotai
+//     let token = store.get(tokenAtom);
+
+    
+//     if (!token) {
+//       token = localStorage.getItem("token");
+//     }
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// // Interceptor pour gérer les erreurs globalement
+// api.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     const status = error?.response?.status;
+
+   
+//     if (status === 401) {
+//       console.warn("🔒 Unauthorized - token invalide ou expiré");
+
+     
+//     }
+
+//     // 💥 erreur serveur
+//     if (status >= 500) {
+//       console.error("💥 Server error");
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
+
+
 import axios from "axios";
 import { getDefaultStore } from "jotai";
 import { tokenAtom } from "../atoms/auth.atom";
